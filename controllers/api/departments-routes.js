c3e00ef06449db32c1fbcf08ddf023bbbfbd03bf
PurviMec <princy.mecwan94@gmail.com>
2021-11-27 15:43:34 -0500

    new get route with id created

--- controllers/api/departments-routes.js
+++ controllers/api/departments-routes.js
@@ -25,4 +25,23 @@ router.post('/', (req, res) => {
       });
 });
 
+router.get('/:id', (req, res) => {
+    Departments.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbDeptartmentData => {
+            if(!dbDeptartmentData){
+                res.status(404).json({ message: 'No department found with this id'})
+                return;
+            }
+            res.json(dbDeptartmentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 module.exports = router;
