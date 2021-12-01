feb5a50cd387085d9bf5944f0723300a5d17ca3d
PurviMec <princy.mecwan94@gmail.com>
2021-12-01 17:18:45 -0500

    get route and template for specialist is created

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Specialists } = require('../models');
 
 router.get('/', (req, res) => {
   res.render('homepage');
@@ -18,6 +20,26 @@ router.get('/Medisearch/Specialists', (req, res) => {
 
 router.get('/Medisearch/Specialists/add', (req, res) => {
   res.render('SpecialistForm');
+});
+
+router.get('/Medisearch/Specialists/View', (req, res) => {
+  console.log('==============');
+    Specialists.findAll({
+        attributes: [
+            'id',
+            'SpecialistName',
+            'Speciality'
+        ]
+    })
+        .then(dbSpecialistData => {
+            const specialists = dbSpecialistData.map(specialist => specialist.get({ plain: true }));
+
+            res.render('SpecialistView', {specialists});
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.get('/Medisearch/PatientJourney', (req, res) => {
