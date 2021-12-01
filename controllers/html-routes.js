f649bc7c45939b3e25b3a57113ded0e5235e1637
PurviMec <princy.mecwan94@gmail.com>
2021-12-01 18:20:05 -0500

    created front end to get specialist with id

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -42,6 +42,34 @@ router.get('/Medisearch/Specialists/View', (req, res) => {
         });
 });
 
+router.get('/Medisearch/Specialists/View/:id', (req, res) => {
+  console.log('==============');
+    Specialists.findOne({
+        where: {
+          id: req.params.id
+        },
+        attributes: [
+            'id',
+            'SpecialistName',
+            'Speciality'
+        ]
+    })
+        .then(dbSpecialistData => {
+            if(!dbSpecialistData){
+              res.status(400).json({ message: 'No specialist found with this id' });
+              return;
+            }
+            const specialist = dbSpecialistData.get({ plain: true });
+            res.render('SpecialistViewOne', {
+              specialist
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 router.get('/Medisearch/PatientJourney', (req, res) => {
   res.render('Medisearch');
 });
