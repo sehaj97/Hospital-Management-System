1e978ed34e721dda4a868bfed4d060f6bcec0e1a
Ravneet Panglia <ravneet.panglia@gmail.com>
2021-12-02 17:33:01 -0500

    patients homepage working - functionailty pending

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -131,12 +131,47 @@ router.get('/Medisearch/PatientJourney', (req, res) => {
 });
 
 router.get('/Medisearch/Patients', (req, res) => {
-  res.render('Patients');
+  if (req.session.loggedIn) {
+    res.render('Patients', {
+      loggedIn: req.session.loggedIn
+    });
+    return;
+  }
+  res.render('Login');
 });
 
 router.get('/Medisearch/Patients/add', (req, res) => {
-  res.render('PatientForm');
+  if (req.session.loggedIn) {
+    res.render('PatientForm', {
+      loggedIn: req.session.loggedIn
+    });
+    return;
+  }
+  res.render('Login');
 });
+
+router.get('/Medisearch/Patients/View', (req, res) => {
+  console.log('==============');
+    Patients.findAll({
+        attributes: [
+            'Patient_id',
+            'PatientName',
+            'PatientStatus'
+        ]
+    })
+        .then(dbPatientData => {
+            const patients = dbPatientData.map(patient => patient.get({ plain: true }));
+
+            res.render('PatientsView', {patients});
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+
+
 
 router.get('/Medisearch/Departments', (req, res) => {
   if (req.session.loggedIn) {
