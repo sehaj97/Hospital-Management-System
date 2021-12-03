4c3f7943cc1181635d0aaf37569ea540688ffb44
Ravneet Panglia <ravneet.panglia@gmail.com>
2021-12-02 22:45:11 -0500

    patient edit functionality complete

--- public/javascript/patientUpdate.js
+++ public/javascript/patientUpdate.js
@@ -1,16 +1,25 @@
-async function patientEditFormHandler(event) {
+async function updateFormHandler(event) {
+    event.preventDefault();
+    const patientId = document.querySelector('#patient-id').value.trim();
+
+    window.location.replace('/Medisearch/Patients/edit/'+ patientId);
+}
+ 
+
+  async function patientEditFormHandler(event) {
     event.preventDefault();
     const PatientName = document.querySelector('#patient-name').value.trim();
-    const PatientStatus = document.querySelector('#pateint-status').value.trim();
+    const PatientStatus = document.querySelector('#patient-status').value.trim();
     const PatientType = document.querySelector('#patient-type').value.trim();
     const prescription = document.querySelector('#prescription').value.trim();
     const diagnosis = document.querySelector('#diagnosis').value.trim();
     const reports = document.querySelector('#reports').value.trim();
     const isVaccinated = document.querySelector('#isVaccinated').value.trim();
+    //const id = document.getElementById('input').value;
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
- 
+
     if (PatientName && PatientStatus && PatientType && prescription && diagnosis && reports && isVaccinated) {
         const response = await fetch(`/api/patients/${id}`, {
           method: 'PUT',
@@ -32,5 +41,10 @@ async function patientEditFormHandler(event) {
         }
       }
 }
- 
-document.querySelector('.patient-form').addEventListener('submit', patientEditFormHandler);
+
+if(document.querySelector('.patient-form')){
+    document.querySelector('.patient-form').addEventListener('submit', patientEditFormHandler);
+}
+if(document.querySelector('.update-form')){
+    document.querySelector('.update-form').addEventListener('submit', updateFormHandler);
+}
